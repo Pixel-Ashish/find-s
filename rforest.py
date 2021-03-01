41b9ee2b6199f94ca0f258f83dfea26cb3e5c4ea
Pixel-Ashish <57537268+Pixel-Ashish@users.noreply.github.com>
2021-03-01 11:17:02 +0530

    Add files via upload

---
+++ rforest.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+dataset = pd.read_csv("C:\\Users\\aakas\\Desktop\\petrol_consumption.csv")
+dataset
+dataset.head()
+X = dataset.iloc[:, 0:4].values
+y = dataset.iloc[:, 4].values
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# Feature Scaling
+
+
+
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=200, random_state=0)
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+from sklearn import metrics
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
